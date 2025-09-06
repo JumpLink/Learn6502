b5ca7d8b3d68e83529ac298e0d192acffed0e59c
Pascal Garber <pascal@artandcode.studio>
2025-09-06 08:53:34 +0200

    Gnome: Implement FAB for main action button and separate run menu
    - Move MainButton from header bar to floating action button (FAB) overlay
    - Create new RunMenuButton widget with run menu actions
    - Position FAB in bottom-right corner with proper margins
    - Update widget exports to include new RunMenuButton
    - Remove run menu from MainButton as it now serves as FAB

--- packages/app-gnome/src/widgets/index.ts
+++ packages/app-gnome/src/widgets/index.ts
@@ -1,6 +1,6 @@
 export * from "./debugger/index.ts";
 export * from "./game-console/index.ts";
-
 export * from "./main-button.ts";
+export * from "./run-menu-button.ts";
 export * from "./source-view.ts";
 export * from "./theme-selector.ts";
