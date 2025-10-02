7e4a44e1892f474d06b71f604c84ef00caaf02f9
Pascal Garber <pascal@artandcode.studio>
2025-10-02 19:44:50 +0200

    Refactor application structure and enhance language handling
    - Removed unused exports from Android app types and utils.
    - Added GLib import to GNOME application for improved language change handling.
    - Implemented a restart mechanism for the application upon language change.
    - Enhanced LanguageService to manage language settings more effectively, including signal management for language changes.
    - Introduced new utility functions for property observation in common UI package, improving asynchronous handling of object properties.

--- packages/common-ui/src/index.ts
+++ packages/common-ui/src/index.ts
@@ -2,5 +2,6 @@ export * from "./controller";
 export * from "./data";
 export * from "./services";
 export * from "./types";
+export * from "./utils";
 export * from "./views";
 export * from "./widgets";
